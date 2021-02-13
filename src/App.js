3a46443d63a12c80feb15268ee0eb17ca69302ae
Mystic Kitty <djabranton@gmail.com>
2021-02-12 21:43:07 -0800

    Fleshed out nav w/map function.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import About from './components/About';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <About></About>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
