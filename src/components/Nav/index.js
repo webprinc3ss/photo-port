5342badc4e6b3aefb8d0836874ff6880548f2ce0
Mystic Kitty <djabranton@gmail.com>
2021-02-12 22:18:00 -0800

    Created nav bar.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,6 +14,9 @@ function Nav() {
             description: "Fields, farmhouses, waterfalls, and the beauty of nature",
         },
     ];
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
     return (
         <header>
             <h2>
@@ -36,7 +39,7 @@ function Nav() {
                             className="mx-1"
                             key={category.name}
                         >
-                            <span>
+                            <span onClick={() => categorySelected(category.name)} >
                                 {category.name}
                             </span>
                         </li>
