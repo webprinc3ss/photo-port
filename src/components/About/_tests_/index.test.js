36081963b719e5c5a6c0a61c5a206bc740f446d2
Mystic Kitty <djabranton@gmail.com>
2021-02-14 10:20:14 -0800

    Created react-tests.

--- src/components/About/_tests_/index.test.js
+++ src/components/About/_tests_/index.test.js
@@ -3,7 +3,7 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 describe('About component', () => {
     // First Test
@@ -12,4 +12,9 @@ describe('About component', () => {
     });
 
     // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
 })
