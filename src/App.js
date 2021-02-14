aac31869cd5c41aeebbb51e43bdbbcab241d889d
Mystic Kitty <djabranton@gmail.com>
2021-02-14 14:02:24 -0800

    Started Gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from "./components/Gallery";
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
