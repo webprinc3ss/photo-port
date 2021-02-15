dd6901e7941ff5cdba39b7a76e5c5b2eb67209dc
Mystic Kitty <djabranton@gmail.com>
2021-02-15 11:42:24 -0800

    Added useEffect to change DOM.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
+
     const {
         categories = [],
         setCurrentCategory,
         currentCategory,
     } = props;
+
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+
 
     return (
         <header className="flex-row px-1">
