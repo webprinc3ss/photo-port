5f111839f1b952cedf28fa8c2b60cfd6942dedbf
Mystic Kitty <djabranton@gmail.com>
2021-02-15 18:21:05 -0800

    Added contact form to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact'
 // import './App.css'; //not in code-snapshot
 
 function App() {
@@ -25,10 +26,9 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
