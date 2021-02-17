83d89e094ef4e73f5e7b33f68ef5b62b81c249f3
Mystic Kitty <djabranton@gmail.com>
2021-02-17 08:56:59 -0800

    Commit

--- src/components/Contact/_tests_/index.test.js
+++ src/components/Contact/_tests_/index.test.js
@@ -2,27 +2,27 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Contact from '..';
+import ContactForm from '..';
 
 afterEach(cleanup)
 
 describe('Contact component renders', () => {
     it('renders', () => {
-        render(<Contact />);
+        render(<ContactForm />);
     });
 
     it('renders', () => {
-        const { asFragment } = render(<Contact />)
+        const { asFragment } = render(<ContactForm />)
         expect(asFragment()).toMatchSnapshot()
     })
 
     it('renders', () => {
-        const { getByTestId } = render(<Contact />)
+        const { getByTestId } = render(<ContactForm />)
         expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
     })
 
     it('renders', () => {
-        const { getByTestId } = render(<Contact />)
+        const { getByTestId } = render(<ContactForm />)
         expect(getByTestId('button')).toHaveTextContent('Submit')
     })
 })
