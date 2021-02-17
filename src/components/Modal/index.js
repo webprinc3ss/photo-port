574f95efc6fe6c3bb881c9481ec3d72c9895b785
Mystic Kitty <djabranton@gmail.com>
2021-02-17 10:29:06 -0800

    Added  close modal function.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-function Modal({ currentPhoto }) {
-    const { name, category, description, index } = currentPhoto;
+const Modal = ({ onClose, currentPhoto }) => {
+    const { name, description, category, index } = currentPhoto;
+
     return (
-        //3SX
         <div className="modalBackdrop">
             <div className="modalContainer">
-                <h3 className="modalTitle">{name}</h3>
+                <h3 className="modalTitle">{name} </h3>
                 <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
-                <p>{description}</p>
-                <button type="button">
-                    Close the modal
-                </button>
+                <p>
+                    {description}
+                </p>
+                <button type="button" onClick={onClose}>
+                    Close this modal
+        </button>
             </div>
         </div>
     );
-}
+};
 
 export default Modal;
