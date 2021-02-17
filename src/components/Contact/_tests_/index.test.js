f8d6d96b4f3ebcc9219f22c48adf5f338af2d0d5
Mystic Kitty <djabranton@gmail.com>
2021-02-17 10:50:31 -0800

    Added test for modal.

--- src/components/Contact/_tests_/index.test.js
+++ src/components/Contact/_tests_/index.test.js
@@ -2,29 +2,28 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import ContactForm from '..';
+import Contact from '..'
 
 afterEach(cleanup)
 
-describe('Contact component renders', () => {
+describe('Contact component', () => {
     it('renders', () => {
-        render(<ContactForm />);
+        render(<Contact />);
     });
 
     it('renders', () => {
-        const { asFragment } = render(<ContactForm />)
+        const { asFragment } = render(<Contact />)
         expect(asFragment()).toMatchSnapshot()
-    })
-
-    it('renders', () => {
-        const { getByTestId } = render(<ContactForm />)
-        expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
-    })
-
-    it('renders', () => {
-        const { getByTestId } = render(<ContactForm />)
-        expect(getByTestId('button')).toHaveTextContent('Submit')
     })
 })
 
 
+it('renders', () => {
+    const { getByTestId } = render(<Contact />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+
+it('renders', () => {
+    const { getByTestId } = render(<Contact />)
+    expect(getByTestId('button')).toHaveTextContent('Submit')
+})
